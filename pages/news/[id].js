f8e8e5fd40bcc66d3925a41693f39215a276a2ef
DrGreeny <stefan.timter@googlemail.com>
2023-05-25 22:31:02 +0200

    news banner

--- pages/news/[id].js
+++ pages/news/[id].js
@@ -20,29 +20,39 @@ const NewsPage = () => {
   }
 
   return (
-    <div className="flex">
-      <div className=" w-1/2 m-auto px-10 flex justify-center">
-        <div className="whitespace-pre-line text-gray-200">
-          <h1 className="text-4xl mb-4">{selectedNews?.headline}</h1>
-          <p className="whitespace-pre-line">
-            {ReactHtmlParser(selectedNews?.text)}
-          </p>
-        </div>
+    <div className="flex-col">
+      <div className="relative w-full h-32 mt-10">
+        <Image
+          src="/news/banner_1500x500.jpg"
+          alt="Speak"
+          fill
+          className="object-cover"
+        />
       </div>
-      <div
-        className="relative w-1/2 flex justify-center"
-        style={{
-          height: `calc(100vh - 64px)`,
-        }}
-      >
-        <div className="m-auto">
-          <Image
-            src={`/news/${id}.png`}
-            alt="Abomination"
-            width={500}
-            height={500}
-          />
-          <div>Leaked Image by Atum</div>
+      <div className="flex">
+        <div className=" w-1/2 m-auto px-10 flex justify-center">
+          <div className="whitespace-pre-line text-gray-200">
+            <h1 className="text-4xl mb-4">{selectedNews?.headline}</h1>
+            <p className="whitespace-pre-line">
+              {ReactHtmlParser(selectedNews?.text)}
+            </p>
+          </div>
+        </div>
+        <div
+          className="relative w-1/2  flex justify-center"
+          style={{
+            minHeight: `calc(100vh - 232px)`,
+          }}
+        >
+          <div className="m-auto">
+            <Image
+              src={`/news/${id}.png`}
+              alt="Abomination"
+              width={500}
+              height={500}
+            />
+            <div>Leaked Image by Atum</div>
+          </div>
         </div>
       </div>
     </div>
